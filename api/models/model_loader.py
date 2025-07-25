9c94913ebb690f1ec8b4ddf8cb07e70db6b35dde
H'mil Rahlan <hrahlan2@charlotte.edu>
2025-07-24 23:41:52 -0400

    Update model_loader.py

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,14 +1,4 @@
-from . import (
-    orders,
-    order_details,
-    customers,
-    menu_items,
-    order_items,
-    resources,
-    promotions,
-    reviews,
-    payments
-)
+from . import orders, order_details, customers, menu_items, resources, promotions, reviews, payments
 
 from ..dependencies.database import engine
 
@@ -20,6 +10,6 @@ def index():
     menu_items.Base.metadata.create_all(engine)
     order_items.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
-    #promotions.Base.metadata.create_all(engine)
+    promotions.Base.metadata.create_all(engine)
     reviews.Base.metadata.create_all(engine)
-    #payments.Base.metadata.create_all(engine)
+    payments.Base.metadata.create_all(engine)
