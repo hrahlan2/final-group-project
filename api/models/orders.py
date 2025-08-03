efe7163bbe1acb9ac7f9ce3ca05fe4903d04e621
hmilrahlan <hrahlan2@charlotte.edu>
2025-08-02 20:33:23 -0400

    Update orders.py in models with Guest Info

--- api/models/orders.py
+++ api/models/orders.py
@@ -8,13 +8,16 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    customer_id = Column(Integer, ForeignKey("customers.id"))
+    customer_id = Column(Integer, ForeignKey("customers.id"), nullable=True) # nullable for guest order
+    guest_name = Column(String, nullable=True)
+    guest_phone = Column(String, nullable=True)
+    guest_address = Column(String, nullable=True)
     order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
     tracking_number = Column(String, unique=True)
     status = Column(String, default="placed")
     total_price = Column(Float)
 
     customer = relationship("Customer", back_populates="orders")
-    order_details = relationship("OrderDetail", back_populates="order", cascade="all, delete")
+    order_details = relationship("OrderItem", back_populates="order", cascade="all, delete")
     payment = relationship("Payment", back_populates="order", uselist=False)
     promotions = relationship("Promotion", back_populates="order")
