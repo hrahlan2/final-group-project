14c7ac1c881f733275835a5d16a2439421d05cab
Juan Munoz <jmlm0206@gmail.com>
2025-08-07 19:18:39 -0400

    feat: add payments controller, router, endpoints, and pytest

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, reviews, resources, guest_orders
+from . import orders, order_details, reviews, resources, guest_orders, delivery_options, payments
 
 
 def load_routes(app):
@@ -8,3 +8,5 @@ def load_routes(app):
     app.include_router(resources.router)
     app.include_router(guest_orders.router)
     app.include_router(menu_items.router)
+    app.include_router(delivery_options.router)
+    app.include_router(payments.router)
